e1a4a517df1b9d007d422095a8627280580c5d41
Sophea PHOS <52313314+phsophea101@users.noreply.github.com>
2023-11-21 13:51:36 +0700

    Add files via upload

--- config-service/src/test/java/com/example/config/ConfigApplicationTests.java
+++ config-service/src/test/java/com/example/config/ConfigApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.config;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
